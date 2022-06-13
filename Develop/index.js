3a767d222ee42d2e816d5b546cd61957668130d9
Zach Burkhart <zachburkhart216@gmail.com>
2022-06-12 20:08:38 -0400

    started questions

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,38 @@
 const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter a title for your project',
+        validate: titleInput => {
+          if (titleInput) {
+              return true;
+            } else {
+              console.log('You need to enter a project title!');
+              return false;
+            }
+        }
+      },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description for your project',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('You need to enter a description!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'table-of-contents'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
